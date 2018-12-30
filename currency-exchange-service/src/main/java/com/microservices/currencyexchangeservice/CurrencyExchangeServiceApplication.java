be12c46aef772a999beeb78670af44c79df66309
amolpatil7863 <amol.patil7863@gmail.com>
2018-12-30 18:54:18 +0530

    Added zual api gateway service

--- currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/CurrencyExchangeServiceApplication.java
+++ currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/CurrencyExchangeServiceApplication.java
@@ -3,14 +3,28 @@ package com.microservices.currencyexchangeservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.Bean;
+
+import brave.sampler.Sampler;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableHystrix
 public class CurrencyExchangeServiceApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Main class Running");
 		SpringApplication.run(CurrencyExchangeServiceApplication.class, args);
 	}
 
+	@Bean
+	public Sampler defaultSampler(){
+		System.out.println("IN SAMPLER");
+		return Sampler.ALWAYS_SAMPLE;
+	}
+	
+
+	
 }
 
