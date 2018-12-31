14f52f2d41f57b1fb4e5e548e3009796c1514100
amol.patil7863 <amol.patil7863@gmail.com>
2018-12-31 20:03:50 +0530

    added spring-security-service

---
+++ spring-boot-security/src/main/java/com/spring/security/springbootsecurity/SecurityConfiguration.java
@@ -0,0 +1,40 @@
+package com.spring.security.springbootsecurity;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+	
+	@Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {  
+        BCryptPasswordEncoder encoder = passwordEncoder();
+        auth.inMemoryAuthentication().withUser("amol").password(encoder.encode("amol123")).roles("USER");  
+    }
+	
+	 @Bean
+     public BCryptPasswordEncoder passwordEncoder() {
+         return new BCryptPasswordEncoder();
+     }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity
+                .authorizeRequests()
+                .anyRequest()
+                .fullyAuthenticated()
+                //.antMatchers("**/rest/*")
+                .and()
+                //.addFilterBefore(customFilter(), BasicAuthenticationFilter.class)
+                .httpBasic();
+        httpSecurity.csrf().disable();
+
+    }
+}
