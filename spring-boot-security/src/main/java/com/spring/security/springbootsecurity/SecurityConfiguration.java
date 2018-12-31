11e46cfc0b1c771beb99d195a3ad0c34f2b066da
amolpatil7863 <amol.patil7863@gmail.com>
2018-12-31 23:15:11 +0530

    Alternate code added for authentication

--- spring-boot-security/src/main/java/com/spring/security/springbootsecurity/SecurityConfiguration.java
+++ spring-boot-security/src/main/java/com/spring/security/springbootsecurity/SecurityConfiguration.java
@@ -2,26 +2,27 @@ package com.spring.security.springbootsecurity;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 	
-	@Override
+	/*@Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {  
         BCryptPasswordEncoder encoder = passwordEncoder();
         auth.inMemoryAuthentication().withUser("amol").password(encoder.encode("amol123")).roles("USER");  
-    }
+    }*/
 	
-	 @Bean
+	 /*@Bean
      public BCryptPasswordEncoder passwordEncoder() {
          return new BCryptPasswordEncoder();
-     }
+     }*/
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
@@ -37,4 +38,19 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
         httpSecurity.csrf().disable();
 
     }
+    
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("amol")
+                .password("amol123")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+    
+    
 }
